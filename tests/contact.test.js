1729739208205aa3992c4019aa2f9d13bcf6b3db
k2works <kakimomokuri@gmail.com>
2019-04-09 13:33:22 +0900

    refactor: モックオブジェクトの導入
    ref:#MMC-4

--- tests/contact.test.js
+++ tests/contact.test.js
@@ -5,8 +5,17 @@ const test = require('unit.js');
 const app = require('../src/app');
 
 const request = supertest(app);
+const AWS = require("aws-sdk");
+const AWSMock = require('aws-sdk-mock');
 
 describe('Test contact controller', function () {
+  before(() => {
+    AWSMock.setSDKInstance(AWS);
+    AWSMock.mock('DynamoDB', 'createTable', function (params, callback){
+      callback(null, "successfully create table in database");
+    });
+  });
+
   it('問い合わせテーブルを作る', function (done) {
     request.post('/api/create').expect(200).end(function (err, result) {
       test.string(result.body.Message).contains('問い合わせテーブルを作成しました');
