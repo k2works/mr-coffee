6dce00bd10df3e0ffb811c5d0efd283a1097a377
k2works <kakimomokuri@gmail.com>
2019-04-09 17:36:31 +0900

    fix: モックオブジェクトが機能しないデグレーション
    ref:#MMC-4

--- src/repository/DynamodbRepository.js
+++ src/repository/DynamodbRepository.js
@@ -1,21 +1,24 @@
 const config = require('../appConfigDynamodb');
 const contact = require('../model/contact');
-const AWS = config.configAws();
-const dynamodb = new AWS.DynamoDB();
-const dynamodbClient = new AWS.DynamoDB.DocumentClient();
 
 exports.create = async function() {
+  const AWS = config.configAws();
+  const dynamodb = new AWS.DynamoDB();
   console.log("Creating a table...");
   return await dynamodb.createTable(contact.model).promise();
 };
 
 exports.drop = async function() {
+  const AWS = config.configAws();
+  const dynamodb = new AWS.DynamoDB();
   console.log("Dropping a table...");
   return await dynamodb.deleteTable({TableName: contact.model.TableName}).promise();
 };
 
 exports.save = async function(info) {
-  console.log("Adding a new item...");
+  const AWS = config.configAws();
+  const dynamodbClient = new AWS.DynamoDB.DocumentClient();
   const params = contact.create(info);
+  console.log("Adding a new item...");
   return await dynamodbClient.put(params).promise();
 };
