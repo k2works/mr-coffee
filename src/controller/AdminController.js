bb25ef09b86d81ee01e19c643a47a386fe947f1d
k2works <kakimomokuri@gmail.com>
2019-04-10 11:51:41 +0900

    style: 不要なページを削除
    ref:#MMC-4

--- src/controller/AdminController.js
+++ src/controller/AdminController.js
@@ -14,58 +14,6 @@ router.get('/login', (req, res) => {
   res.render("admin/login.html");
 });
 
-router.get('/buttons.html', (req, res) => {
-  res.render("admin/buttons.html");
-});
-
-router.get('/cards.html', (req, res) => {
-  res.render("admin/cards.html");
-});
-
-router.get('/utilities-color.html', (req, res) => {
-  res.render("admin/utilities-color.html");
-});
-
-router.get('/utilities-border.html', (req, res) => {
-  res.render("admin/utilities-border.html");
-});
-
-router.get('/utilities-animation.html', (req, res) => {
-  res.render("admin/utilities-animation.html");
-});
-
-router.get('/utilities-other.html', (req, res) => {
-  res.render("admin/utilities-other.html");
-});
-
-router.get('/login.html', (req, res) => {
-  res.render("admin/login.html");
-});
-
-router.get('/register.html', (req, res) => {
-  res.render("admin/register.html");
-});
-
-router.get('/forgot-password.html', (req, res) => {
-  res.render("admin/forgot-password.html");
-});
-
-router.get('/404.html', (req, res) => {
-  res.render("admin/404.html");
-});
-
-router.get('/blank.html', (req, res) => {
-  res.render("admin/blank.html");
-});
-
-router.get('/charts.html', (req, res) => {
-  res.render("admin/charts.html");
-});
-
-router.get('/tables.html', (req, res) => {
-  res.render("admin/tables.html");
-});
-
 router.get('/contacts.html', async (req, res) => {
   let data = {Items: []};
   let message ='';
